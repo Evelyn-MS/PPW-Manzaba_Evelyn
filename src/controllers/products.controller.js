c87e2b8943e9406690695b23a1cff12a5d452c86
Evelyn-MS <evelyn_manzaba.s@hotmail.com>
2022-03-09 17:54:08 -0500

    Function getProductById

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -16,15 +16,16 @@ export const getProducts = async (req, res) =>{
     res.json(products)
 }
 
-export const getProductById = (req, res) =>{
+export const getProductById = async (req, res) =>{
+    const product = await Product.findById(req.params.productId)
+    res.status(200).json(product)
+}
+
+export const updateProductById = async (req, res) =>{
     
 }
 
-export const updateProductById = (req, res) =>{
-    
-}
-
-export const deleteProductById = (req, res) =>{
+export const deleteProductById = async (req, res) =>{
     
 }
 
