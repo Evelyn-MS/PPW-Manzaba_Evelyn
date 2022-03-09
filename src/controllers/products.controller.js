298ae8abbe8d430107cc951c4bf9c5c47a826d9f
Evelyn-MS <evelyn_manzaba.s@hotmail.com>
2022-03-09 17:04:15 -0500

    Function createProduct

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,7 +1,14 @@
 import Product from '../models/Product'
 
-export const createProduct = (req, res) =>{
-    res.jason('creating product')
+export const createProduct = async (req, res) =>{
+    
+    const {name, category, price, imgURL} = req.body
+
+    const newProduct = new Product({name, category, price, imgURL});
+
+    const productSaved = await newProduct.save()
+
+    res.status(201).json(productSaved)
 }
 
 export const getProducts = (req, res) =>{
