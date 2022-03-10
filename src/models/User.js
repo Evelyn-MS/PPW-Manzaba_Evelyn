3b091bd17b8e3d2fdbbdcaaf7058dc7a59b4149b
Evelyn-MS <evelyn_manzaba.s@hotmail.com>
2022-03-10 15:23:05 -0500

    Roles and Users

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import { Schema, model } from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new Schema(
     {
@@ -21,6 +22,15 @@ const userSchema = new Schema(
     },{
         timestamps: true,
         versionKey: false, 
-})
+});
 
-export default userSchema;
+userSchema.statics.encryptPassword = async (password) => {
+  const salt = await bcrypt.genSalt(10);
+  return await bcrypt.hash(password, salt);
+};
+
+userSchema.statics.comparePassword = async (password, receivedPassword) => {
+  return await bcrypt.compare(password, receivedPassword)
+};
+
+export default model("User", userSchema);
