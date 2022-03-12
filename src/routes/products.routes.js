e542754bec18c26eaaaca262c439e57dcaa86a4b
Evelyn-MS <evelyn_manzaba.s@hotmail.com>
2022-03-12 01:16:34 -0500

    Added Roles, Admin, Moderator

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,17 +1,17 @@
-import {Router} from 'express'
+import {Router} from "express";
+const router = Router();
 
-const router = Router()
+import * as productsCtrl from '../controllers/products.controller';
+import {authJwt} from "../middlewares";
 
-import * as productsCtrl from '../controllers/products.controller'
-
-router.post('/', productsCtrl.createProduct)
+router.post('/', [authJwt.verifyToken, authJwt.inModerator ], productsCtrl.createProduct)
 
 router.get('/', productsCtrl.getProducts)
 
 router.get('/:productId', productsCtrl.getProductById)
 
-router.put('/:productId', productsCtrl.updateProductById)
+router.put('/:productId', authJwt.verifyToken, productsCtrl.updateProductById)
 
-router.delete('/:productId', productsCtrl.deleteProductById)
+router.delete('/:productId', authJwt.verifyToken, productsCtrl.deleteProductById)
 
 export default router;
